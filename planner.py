8d053cf98dea727a31a736b15aa1689113c48996
Alireza Daeijavad <45537267+Daeijavad@users.noreply.github.com>
2020-06-11 17:54:43 +0430

    To open Dailytime.py in the same directory as planner.py & to Not to use pause library

--- planner.py
+++ planner.py
@@ -2,6 +2,6 @@ import os
 import time
 
 while True:
-    exec(open(os.path.dirname(__file__) + '\DailyTime.py').read())
+    exec(open(os.path.dirname(__file__) + 'DailyTime.py').read())
     time.sleep(1200)
     print()
